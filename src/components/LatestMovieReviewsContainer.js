74e6627872adac36ad5a817fc1b4da8eee6068e8
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-07-23 11:52:31 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
+
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'your-key-here';
@@ -7,3 +8,29 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    constructor() {
+        super();
+    
+        this.state = {
+            reviews: []
+        };
+    }
+    
+    componentDidMount() {
+    fetch(URL)
+        .then(response => response.json())
+        .then(response => this.setState({ reviews: response.results }));
+    }
+    
+    render() {
+    return (
+        <div className="latest-movie-reviews">
+        <h2>The Latest Reviews:</h2>
+        <MovieReviews reviews={this.state.reviews} />
+        </div>
+    );
+    }
+}
+
+export default LatestMovieReviewsContainer;
